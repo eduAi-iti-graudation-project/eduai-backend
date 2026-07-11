a0f7554955e9f9f20a592f2f4baf501509a96df1
abdallah-ehab <abdallahehab3710@gmail.com>
2026-07-11 17:07:00 +0300

    feat: add assistant module with chat scaffold

---
+++ src/assistant/assistant.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AssistantService } from './assistant.service';
+import { AssistantController } from './assistant.controller';
+
+@Module({
+  providers: [AssistantService],
+  controllers: [AssistantController],
+})
+export class AssistantModule {}
