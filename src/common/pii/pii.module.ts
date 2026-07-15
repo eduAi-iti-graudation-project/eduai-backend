dc66528c3670ba143e1e9578bcd5d75c7674c520
Alaa Anwer <anwaralaa286@gmail.com>
2026-07-15 13:56:44 +0300

    feat(common): add PII redaction service with module registration

--- src/common/pii/pii.module.ts
+++ src/common/pii/pii.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PiiService } from './pii.service';
 
-@Module({})
+@Global()
+@Module({
+  providers: [PiiService],
+  exports: [PiiService],
+})
 export class PiiModule {}
