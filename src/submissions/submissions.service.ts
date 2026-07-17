a36b4354cc6ef80ebcb62480d1bd168e4b63a85d
abdallah-ehab <abdallahehab3710@gmail.com>
2026-07-17 14:04:10 +0300

    chore: fix prettier formatting in submissions service

--- src/submissions/submissions.service.ts
+++ src/submissions/submissions.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { chunkText } from './chunker';
 import pdfParse from 'pdf-parse';
