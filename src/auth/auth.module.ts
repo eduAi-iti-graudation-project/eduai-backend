7b11ab9e742d0e858ed47a600f782148f38d6a54
abdallah-ehab <abdallahehab3710@gmail.com>
2026-07-22 03:45:36 +0300

    Add SupabaseService to AuthModule providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { SupabaseService } from './supabase.service';
 import { RolesGuard } from './roles.guard';
 
 @Module({
   providers: [
     AuthService,
+    SupabaseService,
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
