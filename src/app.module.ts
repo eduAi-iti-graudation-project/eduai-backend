7e49d6bef833cae8787c90d3751c562758d1cbf9
AhmedAdelSelim01 <ahmedadelll364@gmail.com>
2026-08-01 18:24:57 +0300

    chore: register feedback writer module and mastra tooling
    Wire the module into the app root and bundle the @mastra/core dependency plus jest config and mocks required to build and test the agent.

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { GradesModule } from './grades/grades.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { UsersModule } from './users/users.module';
+import { FeedbackWriterModule } from './feedback-writer/feedback-writer.module';
 
 @Module({
   imports: [
@@ -53,6 +54,7 @@ import { UsersModule } from './users/users.module';
     EnrollmentsModule,
     TeachersModule,
     UsersModule,
+    FeedbackWriterModule,
   ],
   controllers: [AppController],
   providers: [
