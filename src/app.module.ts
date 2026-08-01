515c83461a32509e1848d2afef761c0e14aecf9e
AhmedAdelSelim01 <ahmedadelll364@gmail.com>
2026-08-01 18:29:18 +0300

    feat(homework-helper): add service and REST endpoint
    Expose POST /assistant/homework-help and wire the module into the app root so students can ask for help on assignments.

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { GradesModule } from './grades/grades.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { UsersModule } from './users/users.module';
+import { HomeworkHelperModule } from './homework-helper/homework-helper.module';
 
 @Module({
   imports: [
@@ -53,6 +54,7 @@ import { UsersModule } from './users/users.module';
     EnrollmentsModule,
     TeachersModule,
     UsersModule,
+    HomeworkHelperModule,
   ],
   controllers: [AppController],
   providers: [
